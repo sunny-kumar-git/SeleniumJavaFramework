5abfdd3ccf322cddcd24750243e54800bc0536b3
sunny-kumar <sunnygupta1218@gmail.com>
2020-02-23 23:53:45 +0530

    abhi code

---
+++ AAllPractice/src/prct/ExceptionH.java
@@ -0,0 +1,30 @@
+package prct;
+
+public class ExceptionH {
+
+	private int i=20;
+	private int j;
+	public void setI(int i) {
+		this.i = i;
+	}
+	public void setJ(int j) {
+		this.j = j;
+	}
+	public void setK(int k) {
+		this.k = k;
+	}
+	private int k;
+	public int getI() {
+		return i;
+	}
+	public int getJ() {
+		return j;
+	}
+	public int getK() {
+		return k;
+	}
+
+
+
+}
+
---
+++ AAllPractice/src/prct/MC.java
@@ -0,0 +1,12 @@
+package prct;
+
+public class MC {
+
+	public static void main(String[] args) {
+		ExceptionH e=new ExceptionH();
+		e.setI(20);
+		System.out.println(e.getI());
+
+	}
+
+}
